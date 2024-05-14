cb674a13264e8003520b48a15216007d13c0b412
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2024-05-14 10:55:33 +0200

    Laver en masse UI 14.05.2024

---
+++ Kojg Ragnarock Guide/Pages/Cookies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kojg_Ragnarock_Guide.Pages
+{
+    public class CookiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
